6681e82a192979f9a7eb61887b6eaa8c43a150f4
unknown <damerasanthosh2005@gmail.com>
2025-05-31 16:17:56 +0530

    files are organised

--- backend/index.js
+++ backend/index.js
@@ -30,9 +30,9 @@ mongoose.connection.on('disconnected', () => console.log('Mongoose disconnected'
 
 // App Configuration
 app.set("view engine", "ejs");
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../frontend/views'));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../frontend/public')));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
